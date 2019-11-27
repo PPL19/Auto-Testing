b9ae96ceaf3991179a5cbf6e1f42491aac0170f1
FauzanAr <faurinta12345@gmail.com>
2019-11-27 21:21:11 +0700

    Creating simple test

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-const puppeteer = require('mocha-puppeteer');
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.google.com');
+
+    await browser.close();
+})();
