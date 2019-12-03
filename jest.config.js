6d0a43f5d53aa9d1eaf637f0a70715171968b2f7
FauzanAr <faurinta12345@gmail.com>
2019-12-03 15:41:43 +0700

    Adding dev jest dependencies

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: "jest-puppeteer",
+    globals: {
+      URL: "https://www.google.com/"
+    },
+    // testMatch: [
+    //   "test.js"
+    // ],
+    verbose: true
+}
--- test.js
+++ test.js
@@ -1,15 +1,13 @@
-const puppeteer = require('puppeteer');
+const timeout = process.env.SLOWMO ? 30000 : 10000;
 
-(async function main(){
-    try {
-        const browser = await puppeteer.launch({ headless: false });
-        const page = await browser.newPage();
-        await page.goto('https://www.google.com/', {waitUntil: 'domcontentloaded'});
-        
-        await new Promise(resolve =>  setTimeout(resolve, 5000));
-        console.log('done');
-        await browser.close();
-    } catch (e) {
-        console.log('Err', e);
-    }
-})();
+beforeAll(async () => {
+    await page.goto(URL, {waitUntil: 'domcontentloaded'});
+});
+
+describe('Title test', () => {
+    test('Testing to google title', async () => {
+        const title = await page.title();
+
+        expect(title).toBe('Google');
+    }, timeout);
+});
