95c9620fa7d33970a31b3a7280f3ad71312faa8e
FauzanAr <faurinta12345@gmail.com>
2019-12-03 08:47:57 +0700

    Creating simple puppeteer test

--- test.js
+++ test.js
@@ -1,9 +1,15 @@
 const puppeteer = require('puppeteer');
 
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://www.google.com');
-
-    await browser.close();
+(async function main(){
+    try {
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.goto('https://www.google.com/', {waitUntil: 'domcontentloaded'});
+        
+        await new Promise(resolve =>  setTimeout(resolve, 5000));
+        console.log('done');
+        await browser.close();
+    } catch (e) {
+        console.log('Err', e);
+    }
 })();
