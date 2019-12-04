e4b4b6009fa6b22800a970e9d151d9cccc811872
FauzanAr <faurinta12345@gmail.com>
2019-12-04 21:33:17 +0700

    Test case number 1 done

--- test.js
+++ test.js
@@ -8,9 +8,13 @@ beforeAll(async () => {
 describe('Tugas akhir PPL tercinta :*', () => {
 
     test('1. Klik simple form demo drop down and redirected to https://www.seleniumeasy.com/test/basic-first-form-demo.html ', async () => {
-        await page.click('.dropdown-menu > li:nth-child(0+0)')
+        var inputElement = await page.$('div.container-fluid:nth-child(1) div.row:nth-child(2) nav.navbar.navbar-default div.container div.collapse.navbar-collapse ul.nav.navbar-nav:nth-child(1) li.dropdown:nth-child(1) > a.dropdown-toggle');
+        await inputElement.click();
+        await page.waitForSelector("div.container-fluid:nth-child(1) div.row:nth-child(2) nav.navbar.navbar-default div.container div.collapse.navbar-collapse ul.nav.navbar-nav:nth-child(1) > li.dropdown.open:nth-child(1)");
+        inputElement = await page.$('div.container-fluid:nth-child(1) div.row:nth-child(2) nav.navbar.navbar-default div.container div.collapse.navbar-collapse ul.nav.navbar-nav:nth-child(1) li.dropdown.open:nth-child(1) ul.dropdown-menu li:nth-child(1) > a:nth-child(1)');
+        await inputElement.click();
         await page.waitForNavigation({waitUntil: "domcontentloaded"});
         const title = await page.title();
-        expect(title).toBe('Selenium Easy - Best Demo website to practice Selenium Webdriver Online');
+        expect(title).toBe('Selenium Easy Demo - Simple Form to Automate using Selenium');
     }, timeout);
 });
