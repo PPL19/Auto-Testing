33b1a900dfb2bd04220aac2fd0fefb533220128b
FauzanAr <faurinta12345@gmail.com>
2019-12-04 22:14:45 +0700

    Case number 2 done

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ beforeAll(async () => {
 
 describe('Tugas akhir PPL tercinta :*', () => {
 
-    test('1. Klik simple form demo drop down and redirected to https://www.seleniumeasy.com/test/basic-first-form-demo.html ', async () => {
+    test('1. Click simple form demo drop down and redirected to https://www.seleniumeasy.com/test/basic-first-form-demo.html ', async () => {
         var inputElement = await page.$('div.container-fluid:nth-child(1) div.row:nth-child(2) nav.navbar.navbar-default div.container div.collapse.navbar-collapse ul.nav.navbar-nav:nth-child(1) li.dropdown:nth-child(1) > a.dropdown-toggle');
         await inputElement.click();
         await page.waitForSelector("div.container-fluid:nth-child(1) div.row:nth-child(2) nav.navbar.navbar-default div.container div.collapse.navbar-collapse ul.nav.navbar-nav:nth-child(1) > li.dropdown.open:nth-child(1)");
@@ -16,5 +16,22 @@ describe('Tugas akhir PPL tercinta :*', () => {
         await page.waitForNavigation({waitUntil: "domcontentloaded"});
         const title = await page.title();
         expect(title).toBe('Selenium Easy Demo - Simple Form to Automate using Selenium');
+    }, timeout);
+
+    test('2. Inputing single input field and comparing the result', async () => {
+        const inputer = "Hallo";
+        await page.waitForSelector("#get-input");
+        await page.type('#user-message', inputer, {delay:5})
+        const button = await page.$('div.container-fluid.text-center:nth-child(2) div.row div.col-md-6.text-left:nth-child(2) div.panel.panel-default:nth-child(4) div.panel-body form:nth-child(3) > button.btn.btn-default');
+        await button.click();
+        const result = await page.$eval('#display', el => el.innerHTML);
+        expect(result).toBe(inputer);
+    }, timeout);
+
+    test('3. Inputing multi input field and make "NaN" result', async () => {
+        const a = "a";
+
+        
+
     }, timeout);
 });
